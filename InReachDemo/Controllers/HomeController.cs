e69c73a399c5a0a1b094d03ca0c9bef28c7506ce
Roman Bogushevich <50647859+romanbog@users.noreply.github.com>
2020-06-10 15:10:23 -0700

    Add project files.

---
+++ InReachDemo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using InReachDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace InReachDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string FileName;
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(FileModel UserInput, HttpPostedFileBase uploadFile)
+        {
+            if(uploadFile != null)
+            {
+                //janky, but works
+                UserInput.UserFile = uploadFile;
+
+                ViewBag.Message = UserInput.SendOut();
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "No File found";
+                return View();
+            }
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ InReachDemo/Models/FileModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace InReachDemo.Models
+{
+    public class FileModel
+    {
+        [DisplayName("Input Email: ")]
+        [Required(ErrorMessage = "You must input an email.")]
+        public string Email { get; set; }
+
+        [DisplayName("Select File: ")]
+        [Required(ErrorMessage = "You must upload a file.")]
+        public HttpPostedFileBase UserFile { get; set; }
+
+        private IAmazonS3 s3Client;
+
+        //Uploads UserFile to S3, then sends an email with the link. Returns string for ViewBag
+        public string SendOut()
+        {
+            if (!UploadAWS())
+            {
+                return "AWS upload failed";
+            }
+            if (!SendEmail())
+            {
+                return "Sending Email Failed";
+            }
+            return "Accepted file, your email should arrive shortly! ";
+        }
+
+        //Inspired by https://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjectPreSignedURLDotNetSDK.html
+        private bool UploadAWS()
+        {
+            s3Client = new AmazonS3Client(RegionEndpoint.USWest2);
+            var url = GeneratePreSignedURL();
+
+            return UploadObject(url);
+        }
+
+        private bool UploadObject(string url)
+        {
+            try
+            {
+                HttpWebRequest httpRequest = WebRequest.Create(url) as HttpWebRequest;
+                httpRequest.Method = "PUT";
+                using (Stream dataStream = httpRequest.GetRequestStream())
+                {
+
+                    var buffer = new byte[8000];
+                    var tempFile = UserFile;
+
+                    using (Stream fileStream = tempFile.InputStream)
+                    {
+                        int bytesRead = 0;
+                        while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            dataStream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+                HttpWebResponse response = httpRequest.GetResponse() as HttpWebResponse;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string GeneratePreSignedURL()
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                //ContentType = UserFile.ContentType,
+                
+                BucketName = "romanstempbucket",
+                Key = UserFile.FileName,
+                Verb = HttpVerb.PUT,
+                Expires = DateTime.Now.AddMinutes(5)
+            };
+
+            
+
+            string url = s3Client.GetPreSignedURL(request);
+            return url;
+        }
+
+
+        //Sends an email with generated bucket link.
+        private bool SendEmail()
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                message.To.Add(Email);
+                message.Subject = "Romans File Forwarder: " + UserFile.FileName;
+                message.Body = "https://romanstempbucket.s3.amazonaws.com/" + UserFile.FileName;
+
+                //The plan originally was to use Amazon SES, but that's a pain to set up, so it's SMTP time; 
+                SmtpClient smtp = new SmtpClient();
+                smtp.Send(message);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
